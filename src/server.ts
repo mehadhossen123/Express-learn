8495128d270e9084427512c478255ed8d5ab5a36
Mehad Hossen <mehad004938@gmail.com>
2026-05-14 11:30:15 +0600

    connect postgresql successfully

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
 import express, { type Application, type Request, type Response } from "express";
+import {Pool} from "pg";
 const app:Application=express();
 const port=5000;
+// mddleware
 app.use(express.json())
+
+// connect postgresql with this server ;
+const pool = new Pool({
+  connectionString:
+    "postgresql://neondb_owner:npg_YGKQk6MZVh1X@ep-dry-smoke-apz8nzlw-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+});
+
+//  here we write the all api for whole project 
 app.get("/",(req:Request,res:Response)=>{
    res.send("hello this is express server")
 })
