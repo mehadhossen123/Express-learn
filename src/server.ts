7a5e98c41531c886305c8ab54ac2662af11ec24b
Mehad Hossen <mehad004938@gmail.com>
2026-05-14 16:42:00 +0600

    insert data into table

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express, { type Application, type Request, type Response } from "express"
 import {Pool} from "pg";
 const app:Application=express();
 const port=5000;
-// mddleware
+// middleware
 app.use(express.json())
 
 // connect postgresql with this server ;
@@ -11,6 +11,32 @@ const pool = new Pool({
     "postgresql://neondb_owner:npg_YGKQk6MZVh1X@ep-dry-smoke-apz8nzlw-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 });
 
+const initDB=async ()=>{
+    try{
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY ,
+            name VARCHAR(20),
+            email VARCHAR(20) UNIQUE NOT NULL,
+            password VARCHAR(20) NOT NULL,
+            age INT ,
+            is_active BOOLEAN DEFAULT true,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+
+            )
+            `)
+            console.log("database connected successfully",initDB)
+           
+
+    }
+    catch(error){
+        console.log(error);
+    }
+
+}
+ initDB();
+
 //  here we write the all api for whole project 
 app.get("/",(req:Request,res:Response)=>{
    res.send("hello this is express server")
@@ -18,11 +44,26 @@ app.get("/",(req:Request,res:Response)=>{
 
 // her we have to learn post method 
 app.post("/",async(req:Request,res:Response)=>{
-    const data=req.body;
+   try{
+     const {name,email,password,age}=req.body;
+    const result=await pool.query(`
+        INSERT INTO users(name,email,password,age) 
+        VALUES($1,$2,$3,$4)
+        RETURNING *
+        `,[name,email,password,age])
+        // console.log(result)
     res.status(200).json({
         message:"created",
-        data:data
+        data:result.rows[0]
     })
+    
+   }catch(error:any){
+     res.status(200).json({
+       message:error.message,
+       error:error.detail,
+     });
+
+   }
 })
 
 app.listen(port,()=>{
